3008f9918957f95947596533eb22c5d82e263206
Harun Yahya Ünal <63019370+BirYazilimci74@users.noreply.github.com>
2024-07-10 17:59:25 +0300

    Delegate

---
+++ Small Projects/Delegate/MessageSystem.cs
@@ -0,0 +1,24 @@
+﻿namespace Delegate;
+
+public class MessageSystem
+{
+    public void Hello()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    public void Alert()
+    {
+        Console.WriteLine("Be Carefull");
+    }
+
+    public void Hello2(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Alert2(string alert)
+    {
+        Console.WriteLine(alert);
+    }
+}
---
+++ Small Projects/Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+        public delegate void MyDelegateWithParam(string message);
+
+        private static void Main(string[] args)
+        {
+            MessageSystem messageSystem = new MessageSystem();
+            MyDelegate myDelegate = null;
+
+            myDelegate += messageSystem.Hello;
+            myDelegate += messageSystem.Alert;
+
+            myDelegate();
+
+            Console.WriteLine("*************");
+            myDelegate -= messageSystem.Hello;
+            myDelegate();
+
+            Console.WriteLine("***With Parameter***");
+            MyDelegateWithParam myDelegateWithParam = null;
+
+            myDelegateWithParam += messageSystem.Alert2;
+            myDelegateWithParam += messageSystem.Hello2;
+
+            myDelegateWithParam("HelloWorld");
+
+        }
+    }
+}
