52c69e0934f774a8234c976af5847507a756fe01
Harun Yahya Ünal <63019370+BirYazilimci74@users.noreply.github.com>
2024-07-10 17:58:29 +0300

    Attributes

---
+++ Small Projects/Attributes/Student.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Attributes;
+
+public class Student
+{
+    [RequiredAttribute]
+    public int Id { get; set;}
+    public string Name { get; set;}
+}
