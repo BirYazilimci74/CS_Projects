03047255e5cf0ed132e340f7fc87440a11bc007e
Harun Yahya Ünal <63019370+BirYazilimci74@users.noreply.github.com>
2024-08-08 18:15:02 +0300

    Display Data

--- Form Uygulamaları/LibraryManagementSystemWithEF/Form1.Designer.cs
+++ Form Uygulamaları/LibraryManagementSystemWithEF/Form1.Designer.cs
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.dgvBorrowedBook = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBorrowedBook)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(76, 67);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.RowHeadersWidth = 51;
+            this.dgvBooks.RowTemplate.Height = 24;
+            this.dgvBooks.Size = new System.Drawing.Size(794, 150);
+            this.dgvBooks.TabIndex = 0;
+            this.dgvBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dgvBorrowedBook
+            // 
+            this.dgvBorrowedBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBorrowedBook.Location = new System.Drawing.Point(76, 233);
+            this.dgvBorrowedBook.Name = "dgvBorrowedBook";
+            this.dgvBorrowedBook.RowHeadersWidth = 51;
+            this.dgvBorrowedBook.RowTemplate.Height = 24;
+            this.dgvBorrowedBook.Size = new System.Drawing.Size(794, 150);
+            this.dgvBorrowedBook.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1048, 595);
+            this.Controls.Add(this.dgvBorrowedBook);
+            this.Controls.Add(this.dgvBooks);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBorrowedBook)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.DataGridView dgvBorrowedBook;
     }
 }
 
--- Form Uygulamaları/LibraryManagementSystemWithEF/Form1.cs
+++ Form Uygulamaları/LibraryManagementSystemWithEF/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibraryManagementSystemWithEF.DAL;
 
 namespace LibraryManagementSystemWithEF
 {
     public partial class Form1 : Form
     {
+        private LibraryContext _context = new LibraryContext();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvBooks.DataSource = _context.Books.ToList();
+            dgvBorrowedBook.DataSource = _context.BorrowedBooks.ToList();
         }
     }
 }
