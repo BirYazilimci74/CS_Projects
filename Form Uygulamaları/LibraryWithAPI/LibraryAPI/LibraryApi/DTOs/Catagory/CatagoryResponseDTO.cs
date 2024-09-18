c3964872ae266a67c7bedf47ff69e4526162835d
Harun Yahya Ünal <63019370+BirYazilimci74@users.noreply.github.com>
2024-09-18 17:45:11 +0300

    Moving the project to a another repo

--- Form Uygulamaları/LibraryWithAPI/LibraryAPI/LibraryApi/DTOs/Catagory/CatagoryResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-
-namespace LibraryApi.DTOs.Catagory
-{
-    public class CatagoryDTO
-    {
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- Form Uygulamaları/LibraryWithAPI/LibraryManagementSystemWithAPI/DTOs/Catagory/CatagoryResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-
-namespace LibraryManagementSystemWithAPI.DTOs.Catagory
-{
-    public class CatagoryDTO
-    {
-        public string Name { get; set; } = string.Empty;
-    }
-}
